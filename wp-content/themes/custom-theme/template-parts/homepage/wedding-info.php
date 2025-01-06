8b49eb91624e22f2dc189547055886ce5b21249e
Martin Metodiev <martin.metodiew@gmail.com>
2025-01-06 16:02:03 +0200

    Add a wedding info section

---
+++ wp-content/themes/custom-theme/template-parts/homepage/wedding-info.php
@@ -0,0 +1,79 @@
+<section class="wedding-info-section section-padding30 section-bg"
+         data-background="<?php echo get_asset_image_url( 'gallery/section_bg1.png' ) ?>"
+         style="background-image: url('<?php echo get_asset_image_url( 'gallery/section_bg1.png' ) ?>')">
+    <div class="container">
+        <!-- Section Tittle -->
+        <div class="row d-flex justify-content-center">
+            <div class="col-lg-8">
+                <div class="section-tittle text-center mb-70">
+                    <h2>Wedding Info</h2>
+                    <img src="<?php echo get_asset_image_url( 'gallery/tittle_img.png' ) ?>" alt="">
+                    <p>Quisque nec facilisis sem. In at commodo velit. Aliquam tempor volutpat laoreet. Quisque non
+                        tellus eleifend arcu gravida aliquam. Vivamus quis consequat nisl</p>
+                </div>
+            </div>
+        </div>
+        <div class="row align-items-end">
+            <div class="col-xl-4 col-lg-4 col-md-6">
+                <div class="single-card text-center mb-30">
+                    <div class="card-top">
+                        <span class="flaticon-chart"></span>
+                        <h4>The Ceremony</h4>
+                    </div>
+                    <div class="card-bottom">
+                        <ul class="list-unstyled">
+                            <li><i class="fas fa-calendar-alt"></i>April 20, 2016</li>
+                            <li><i class="far fa-clock"></i>5:30 PM</li>
+                            <li><i class="fas fa-map-marker-alt"></i>The Mayflower Hotel</li>
+                            <li>1127 Connecticut Avenue,NY</li>
+                            <li><i class="far fa-map"></i> Check out the map</li>
+                        </ul>
+                    </div>
+                    <div class="card-buttons mt-30">
+                        <a href="#" class="btn card-btn1">Get Started</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-4 col-lg-4 col-md-6">
+                <div class="single-card active text-center mb-30">
+                    <div class="card-top">
+                        <span class="flaticon-project"></span>
+                        <h4>The Reception</h4>
+                    </div>
+                    <div class="card-bottom">
+                        <ul class="list-unstyled">
+                            <li><i class="fas fa-calendar-alt"></i>April 20, 2016</li>
+                            <li><i class="far fa-clock"></i>5:30 PM</li>
+                            <li><i class="fas fa-map-marker-alt"></i>The Mayflower Hotel</li>
+                            <li>1127 Connecticut Avenue,NY</li>
+                            <li><i class="far fa-map"></i> Check out the map</li>
+                        </ul>
+                    </div>
+                    <div class="card-buttons mt-30">
+                        <a href="#" class="btn card-btn1">Get Started</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-4 col-lg-4 col-md-6">
+                <div class="single-card text-center mb-30">
+                    <div class="card-top">
+                        <span class="flaticon-award"></span>
+                        <h4>The Party</h4>
+                    </div>
+                    <div class="card-bottom">
+                        <ul class="list-unstyled">
+                            <li><i class="fas fa-calendar-alt"></i>April 20, 2016</li>
+                            <li><i class="far fa-clock"></i>5:30 PM</li>
+                            <li><i class="fas fa-map-marker-alt"></i>The Mayflower Hotel</li>
+                            <li>1127 Connecticut Avenue,NY</li>
+                            <li><i class="far fa-map"></i> Check out the map</li>
+                        </ul>
+                    </div>
+                    <div class="card-buttons mt-30">
+                        <a href="#" class="btn card-btn1">Get Started</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
--- wp-content/themes/custom-theme/templates/homepage.php
+++ wp-content/themes/custom-theme/templates/homepage.php
@@ -7,20 +7,9 @@
 <?php get_header(); ?>
 
 <main>
-    <div class="single-slider slider-height hero-overly d-flex align-items-center">
-        <div class="container-xxl">
-            <div class="row d-flex align-items-center">
-                <div class="col-xl-8 col-lg-6 col-md-8 ">
-                    <div class="hero__caption">
-                        <span data-animation="fadeInLeft" data-delay=".3s">january 23</span>
-                        <h1 data-animation="fadeInLeft" data-delay=".5s" data-duration="2000ms">Dennis<strong> & </strong> Judith</h1>
-                        <p data-animation="fadeInLeft" data-delay=".9s">will get married</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
 	<?php get_template_part( 'template-parts/banners/hero-banner' ); ?>
+
+	<?php get_template_part( 'template-parts/homepage/wedding-info' ); ?>
 
 
 </main>
