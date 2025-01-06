88ca9b392b8a0ef70e82c4abb62d3665df72a25d
Martin Metodiev <martin.metodiew@gmail.com>
2025-01-06 16:04:01 +0200

    Add navigation with WP Menu

--- wp-content/themes/custom-theme/header.php
+++ wp-content/themes/custom-theme/header.php
@@ -15,6 +15,32 @@
 
 <header class="header">
     <div class="container-xxl">
-        Header
+        <nav class="navbar navbar-light navbar-expand-lg">
+            <a href="<?php echo esc_url( site_url( '/' ) ); ?>" class="navbar-brand">
+				<?php get_template_part( 'template-parts/header/logo' ); ?>
+            </a>
+
+            <button class="navbar-toggler py-2 px-3" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarCollapse">
+                <span class="fa fa-bars text-primary"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+
+				<?php
+				// Check if the 'header-menu' location has a menu assigned
+				if ( has_nav_menu( 'header-menu' ) ) {
+					wp_nav_menu( array(
+						'theme_location' => 'header-menu',
+						'depth'          => 2,
+						'walker'         => new WP_Bootstrap_Navwalker(),
+						'menu_class'     => 'navbar-nav ms-auto',
+						'container'      => false
+					) );
+				}
+				?>
+
+            </div>
+        </nav>
     </div>
 </header>
