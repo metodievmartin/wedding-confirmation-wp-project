fa3d9a27867218e6be1b2ccce7e5af53dc7f4cb4
Martin Metodiev <martin.metodiew@gmail.com>
2025-01-07 16:55:16 +0200

    Add logo, nav menu and contact us section to the footer

--- wp-content/themes/custom-theme/footer.php
+++ wp-content/themes/custom-theme/footer.php
@@ -1,5 +1,61 @@
 <footer class="main-footer-section">
-	<div class="container-xxl">Footer</div>
+    <div class="container-xxl">
+        <div class="row justify-content-between pt-5 pb-5">
+            <div class="footer-column col-lg-3 col-md-6">
+                <div class="footer-item">
+                    <h2 class="mb-4">
+						<?php get_template_part( 'template-parts/header/logo' ); ?>
+                    </h2>
+                    <p class="lh-lg mb-4">
+                        There cursus massa at urnaaculis estieSed aliquamellus vitae ultrs condmentum leo massamollis
+                        its estiegittis miristum.
+                    </p>
+
+					<?php if ( is_active_sidebar( 'footer-social-icons' ) ) : ?>
+                        <div class="footer-logo-widget-area">
+							<?php dynamic_sidebar( 'footer-social-icons' ); ?>
+                        </div>
+					<?php endif; ?>
+                </div>
+            </div>
+            <div class="footer-column col-xl-3 col-lg-3 col-md-6 mb-30">
+                <h3 class="footer-section-heading"><?php esc_html_e( 'Page Links', 'wedding_confirmation' ); ?></h3>
+
+				<?php
+
+				if ( has_nav_menu( 'footer-page-links-menu' ) ) {
+					wp_nav_menu( array(
+						'theme_location' => 'footer-page-links-menu',
+						'walker'         => new WP_Bootstrap_Navwalker(),
+						'menu_class'     => 'nav flex-column footer-menu',
+						'container'      => false
+					) );
+				}
+
+				?>
+
+            </div>
+            <div class="footer-column col-lg-3 col-md-6">
+                <div class="footer-item">
+                    <h3 class="footer-section-heading"><?php esc_html_e( 'Contact Us', 'wedding_confirmation' ); ?></h3>
+                    <div class="d-flex flex-column align-items-start">
+                        <p><i class="fa fa-map-marker-alt me-2"></i> Test</p>
+                        <p><i class="fa fa-phone-alt me-2"></i> Test</p>
+                        <p><i class="fas fa-envelope me-2"></i> Test</p>
+                        <p><i class="fa fa-clock me-2"></i> 26/7 Hours Service</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="copyright-area">
+        <div class="container-xxl text-center">
+            <p class="copyright-text">Copyright &copy; <?php echo date( "Y" ); ?> Wedding Confirmation. All Rights
+                Reserved</p>
+        </div>
+    </div>
+
 </footer>
 
 <?php wp_footer(); ?>
