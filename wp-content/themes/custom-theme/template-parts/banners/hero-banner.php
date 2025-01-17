b2305729c398a1305f1ba688031dc4b18cf8e16e
Martin Metodiev <martin.metodiew@gmail.com>
2025-01-17 22:43:13 +0200

    Refactor and improve hero banner styling

--- wp-content/themes/custom-theme/template-parts/banners/hero-banner.php
+++ wp-content/themes/custom-theme/template-parts/banners/hero-banner.php
@@ -20,30 +20,33 @@ $banner_image             = $banner_fields['hero_banner_image'] ?? null;
         background-image: url(<?php echo get_asset_image_url('/hero/hero_shape1.png') ?>);
     }
 
-    .main-hero-title::after {
+    .main-hero-title .name.second::after {
         background-image: url(<?php echo get_asset_image_url('/hero/hero_shape2.png') ?>);
     }
 
 </style>
 
 <section class="hero-banner page-section py-6 my-6 mt-0">
-    <div class="container-xxl hero-banner-container">
+    <div class="container-lg hero-banner-container">
         <div class="row align-items-center">
-            <div class="title-container col-lg-7 col-md-12">
+            <div class="title-container">
 
 				<?php if ( ! empty( $hero_banner_wedding_date ) ): ?>
-                    <h3 class="mb-4 wedding-date-heading">
+                    <h3 class="wedding-date-heading">
 						<?php echo $hero_banner_wedding_date; ?>
                     </h3>
 				<?php endif; ?>
 
 				<?php if ( ! empty( $hero_banner_first_name ) && ! empty( $hero_banner_second_name ) ): ?>
-                    <h1 class="display-1 main-hero-title"><?php echo $hero_banner_first_name; ?><strong>
-                            & </strong> <?php echo $hero_banner_second_name; ?></h1>
+                    <h1 class="main-hero-title">
+                        <span class="name first"><?php echo $hero_banner_first_name; ?></span>
+                        <span class="ampersand"> & </span>
+                        <span class="name second"><?php echo $hero_banner_second_name; ?></span>
+                    </h1>
 				<?php endif; ?>
 
 				<?php if ( ! empty( $banner_secondary_title ) ): ?>
-                    <h5 class="text-uppercase fw-lighter m-0 py-3">
+                    <h5 class="text-uppercase secondary-hero-title m-0 py-3">
 						<?php echo esc_html( $banner_secondary_title ); ?>
                     </h5>
 				<?php endif; ?>
