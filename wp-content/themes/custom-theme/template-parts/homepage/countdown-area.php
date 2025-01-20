ed753a0211358e441df58fb501b4eaf81ed57a49
Martin Metodiev <martin.metodiew@gmail.com>
2025-01-20 17:16:13 +0200

    Add conditional messaging based on whether the end date has passed or not

--- wp-content/themes/custom-theme/template-parts/homepage/countdown-area.php
+++ wp-content/themes/custom-theme/template-parts/homepage/countdown-area.php
@@ -1,10 +1,30 @@
-<section class="countdown-section">
+<?php
+$confirmations_end_date = wccf()->get_confirmations_end_date();
+$date_string            = wccf()->get_confirmations_end_date_formatted( 'c' );
+$has_end_date_passed    = wccf()->has_end_date_passed();
+?>
+
+<section id="confirm-attendance-section" class="countdown-section">
     <div class="container-lg">
         <div class="row d-flex justify-content-center">
             <div class="col-lg-8">
-                <div class="section-tittle text-center mb-70">
-                    <h2>Confirmation Closes In:</h2>
+                <div class="section-tittle text-center">
+					<?php if ( ! $has_end_date_passed ) : ?>
+                        <h2>Confirmation Closes In:</h2>
+					<?php else: ?>
+                        <h2>Confirmation Closed:</h2>
+					<?php endif; ?>
+
                     <img src="<?php echo get_asset_image_url( 'gallery/tittle_img.png' ) ?>" alt="">
+
+					<?php if ( ! empty( $confirmations_end_date ) && ! $has_end_date_passed ) : ?>
+                        <p>
+                            We kindly ask you to confirm or decline your attendance
+                            until <?php echo $confirmations_end_date->format( 'H:i:s d/m/Y' ); ?>
+                        </p>
+					<?php else: ?>
+                        <p>We are no longer accepting new confirmations. Thank you!</p>
+					<?php endif; ?>
                 </div>
             </div>
         </div>
@@ -12,13 +32,13 @@
             <div class="col-lg-12 col-md-12 no-padding-until-lg">
                 <div id="wccf-countdown"
                      class="count-down-wrapper"
-                     data-end-date="<?php echo wccf()->get_countdown_date() ?>">
+                     data-end-date="<?php echo $date_string ?>">
                     <div class="digits-container d-flex justify-content-between flex-wrap">
                         <div class="single-counter text-center">
                             <span id="cd-days" class="counter">23</span>
                             <p>days</p>
                         </div>
-                        <div class="single-counter active text-center">
+                        <div class="single-counter text-center">
                             <span id="cd-hours" class="counter">15</span>
                             <p>hours</p>
                         </div>
