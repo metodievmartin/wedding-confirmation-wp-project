be980fc89217e318495c5f3d9df020c446dc154c
Martin Metodiev <martin.metodiew@gmail.com>
2025-01-20 16:37:42 +0200

    Add new helper methods to work with the confirmations end date

--- wp-content/plugins/wedding-confirmation-custom-functionality/includes/services/class-settings-service.php
+++ wp-content/plugins/wedding-confirmation-custom-functionality/includes/services/class-settings-service.php
@@ -29,6 +29,45 @@ class WCCF_Settings_Service {
 		return null;
 	}
 
+	public function get_confirmations_end_date_formatted( string $format, bool $convert_to_local_time = true ): string {
+		$date = $this->get_confirmations_end_date( $convert_to_local_time );
+
+		if ( empty( $date ) || empty( $format ) ) {
+			return '';
+		}
+
+		return $date->format( $format );
+	}
+
+
+	/**
+	 * Check if the end date has already passed.
+	 *
+	 * @return bool True if the end date has passed, false otherwise.
+	 */
+	public function has_confirmations_end_date_passed(): bool {
+		$end_date = get_option( self::CONFIRMATIONS_END_DATE );
+		$timezone = get_option( self::CONFIRMATIONS_END_DATE_TIMEZONE );
+
+		if ( empty( $end_date ) || empty( $timezone ) ) {
+			// If the end date or timezone is not set, assume it hasn't passed
+			return false;
+		}
+
+		try {
+			$tz_object         = new DateTimeZone( $timezone );
+			$end_date_time     = new DateTime( $end_date, $tz_object );
+			$current_date_time = new DateTime( 'now', $tz_object );
+
+			// Compare the current time with the end date
+			return $current_date_time > $end_date_time;
+		} catch ( Exception $e ) {
+			error_log( 'Error checking end date: ' . $e->getMessage() );
+
+			return false;
+		}
+	}
+
 	public function save_confirmations_end_date( string $date ): bool {
 		return update_option( self::CONFIRMATIONS_END_DATE, $date );
 	}
