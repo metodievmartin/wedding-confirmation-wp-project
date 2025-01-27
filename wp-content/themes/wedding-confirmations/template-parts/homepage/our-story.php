279d7617095a45fad16fd9d97e759614a33bc721
Martin Metodiev <martin.metodiew@gmail.com>
2025-01-27 20:11:01 +0200

    Refactor our-story section and add new styling

--- wp-content/themes/wedding-confirmations/template-parts/homepage/our-story.php
+++ wp-content/themes/wedding-confirmations/template-parts/homepage/our-story.php
@@ -9,26 +9,21 @@ $our_story_card_image       = $our_story_section_fields['our_story_card_image'] 
 
 <section id="our-story-section" class="our-story-section story-padding page-section">
     <div class="container-lg">
-        <!-- Section Tittle -->
-        <div class="row justify-content-center">
-            <div class="col-xl-7 col-lg-8">
-                <div class="section-tittle text-center">
-                    <h2>Our Love Story</h2>
-                    <img src="<?php echo get_asset_image_url( 'gallery/tittle_img.png' ) ?>" alt="">
 
+		<?php
+		$section_title_args = array(
+			'section_title'    => __( 'Our Love Story', 'wedding-confirmations' ),
+			'section_subtitle' => $our_story_section_subtitle
+		);
 
-					<?php if ( ! empty( $our_story_section_subtitle ) ): ?>
+		get_template_part( 'template-parts/components/section-title', null, $section_title_args );
 
-                        <p><?php echo esc_html( $our_story_section_subtitle ); ?></p>
+		?>
 
-					<?php endif; ?>
-                </div>
-            </div>
-        </div>
-        <div class="row justify-content-center">
-            <div class="col-lg-5">
+        <div class="row g-0 justify-content-center">
+            <div class="col-10 col-sm-7 col-md-6 col-lg-5">
                 <div class="story-caption background-img"
-                     style="background-image: url(<?php echo get_asset_image_url( 'gallery/story2.jpg' ) ?>);">
+                     style="background-image: url('<?php echo get_asset_image_url( '/bg/bg-pattern-25.png' ) ?>');">
                     <div class="story-details">
                         <h4><?php echo esc_html( $our_story_card_title ); ?></h4>
 
@@ -39,9 +34,24 @@ $our_story_card_image       = $our_story_section_fields['our_story_card_image'] 
 						<?php endif; ?>
 
                     </div>
+
+                    <img class="corner-ornament top right"
+                         src="<?php echo get_asset_image_url( '/ornaments/corner-ornament-3.svg' ); ?>"
+                         alt="corner-ornament-image">
+
+                    <img class="corner-ornament bottom right"
+                         src="<?php echo get_asset_image_url( '/ornaments/corner-ornament-3.svg' ); ?>"
+                         alt="corner-ornament-image">
+
+                    <img class="flower-ornament"
+                         src="<?php echo get_asset_image_url( '/flower/main-flower.png' ); ?>"
+                         alt="corner-ornament-image">
+
                 </div>
+
+
             </div>
-            <div class="col-lg-5">
+            <div class="col-10 col-sm-7 col-md-6 col-lg-5">
                 <div class="story-img">
 					<?php if ( ! empty( $our_story_card_image ) && isset( $our_story_card_image['sizes'] ) ): ?>
 
